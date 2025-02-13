4f94ab2aa1913c025d3fffd531c43f1e162d8d3c
Leandro Silva <leandro.silva@canadalifegroup.com>
2025-02-13 14:14:42 +0000

    storage account update

--- create-terraform-storage.sh
+++ create-terraform-storage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-RESOURCE_GROUP_NAME="TEAMC-rg"
-STORAGE_ACCOUNT_NAME="TEAMC-sa"
+RESOURCE_GROUP_NAME="teamcrg"
+STORAGE_ACCOUNT_NAME="teamcsa"
 
 # Check if Resource Group exists
 RESOURCE_GROUP_EXISTS=$(az group exists --name $RESOURCE_GROUP_NAME)
@@ -10,7 +10,7 @@ if [ "$RESOURCE_GROUP_EXISTS" = "true" ]; then
   echo "Resource group $RESOURCE_GROUP_NAME already exists."
 else
   # Create Resource Group
-  az group create -l germanywestcentral -n $RESOURCE_GROUP_NAME
+  az group create -l uksouth -n $RESOURCE_GROUP_NAME
 fi
 
 # Check if Storage Account exists
@@ -20,8 +20,8 @@ if [ "$STORAGE_ACCOUNT_EXISTS" = "false" ]; then
   echo "Storage account $STORAGE_ACCOUNT_NAME is already created in resource group $RESOURCE_GROUP_NAME."
 else
   # Create Storage Account
-  az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l germanywestcentral --sku Standard_LRS
+  az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
 
   # Create Storage Account blob
-  az storage container create --name TEAMC --account-name $STORAGE_ACCOUNT_NAME
+  az storage container create --name tfstate --account-name $STORAGE_ACCOUNT_NAME
 fi
