4afea27ebc5fc3a4fe3c5285a99ab597661791d9
Leandro Silva <leandro.silva@canadalifegroup.com>
2025-02-14 09:57:13 +0000

    Created new variablestf, cleaned up maintf

--- TFFiles/VNET/main.tf
+++ TFFiles/VNET/main.tf
@@ -1,20 +1,3 @@
-# Define variables
-variable "resource_group_name" {
-  description = "The name of the resource group"
-}
-
-variable "location" {
-  description = "The location of the resources"
-}
-
-variable "admin_username" {
-  description = "The admin username for the VMs"
-}
-
-variable "admin_password" {
-  description = "The admin password for the VMs"
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
