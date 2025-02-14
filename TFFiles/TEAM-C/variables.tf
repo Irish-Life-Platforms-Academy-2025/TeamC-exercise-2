79437fe8fd5b1d4c0f9c99accabac96559089bbd
Leandro Silva <leandro.silva@canadalifegroup.com>
2025-02-14 14:18:43 +0000

    New NSG changes

--- TFFiles/TEAM-C/variables.tf
+++ TFFiles/TEAM-C/variables.tf
@@ -1,16 +1,20 @@
 # Define variables
 variable "resource_group_name" {
   description = "The name of the resource group"
+  type        = string
 }
 
 variable "location" {
-  description = "The location of the resources"
+  description = "The location of the resource group"
+  type        = string
 }
 
 variable "admin_username" {
   description = "The admin username for the VMs"
+  type        = string
 }
 
 variable "admin_password" {
   description = "The admin password for the VMs"
+  type        = string
 }
